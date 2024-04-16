b13900925fe880ca641911923e130070afeb201b
RadonZ <supachai27102000@gmail.com>
2024-04-17 01:52:05 +0700

    remove unused pic and add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,9 +2,28 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
+    <footer>
     <div className={styles.container}>
-      <div className={styles.top}></div>
-      <div className={styles.bottom}></div>
+      <div className={styles.top}>
+        <p>&copy;2023 Yourcompany</p>
+        <h2>Landing</h2>
+        <a>Purchase now</a>
+      </div>
+      <div className={styles.bottom}>
+        <ul className={styles.menu}>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+        <ul className={styles.social}>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+          <li><a href=""></a></li>
+        </ul>
+      </div>
     </div>
+    </footer>
   )
 }
