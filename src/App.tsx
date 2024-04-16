591c6e75279b81c72e0bccbacf3e34d2ace4c882
RadonZ <supachai27102000@gmail.com>
2024-04-16 16:33:12 +0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import LandingPage1 from './components/LandingPage/LandingPage1';
+import LandingPage2 from './components/LandingPage/LandingPage2 copy 2';
+import LandingPage3 from './components/LandingPage/LandingPage3';
+import LandingPage4 from './components/LandingPage/LandingPage4';
+import Price from './components/Price/Price';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar/>
+    <Header/>
+    <LandingPage1/>
+    <LandingPage2/>
+    <LandingPage3/>
+    <LandingPage4/>
+    <Price/>
+    <Footer/>
     </>
   )
 }
-
-export default App
